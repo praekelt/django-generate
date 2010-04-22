28ec3489d996420ac3499cb7ba9077536506128b
Shaun Sephton <shaun@praekelt.com>
2010-04-22 17:16:01 +0200

    added images

---
+++ generate/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+script_path = os.path.dirname( os.path.realpath( __file__ ) )
+IMAGES = ['%s/resources/1.jpg' % script_path,
+          '%s/resources/2.jpg' % script_path,
+          '%s/resources/3.jpg' % script_path,
+          '%s/resources/4.jpg' % script_path,
+          '%s/resources/5.jpg' % script_path,
+          '%s/resources/6.jpg' % script_path,
+          '%s/resources/7.jpg' % script_path]

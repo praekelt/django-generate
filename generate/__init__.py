cf6e4f67f7ef107cf120dcae76e4d9395f8f917d
euan <euan@praekelt.com>
2010-06-26 11:59:35 +0200

    Added default CSV, PDF & TXT files.

--- generate/__init__.py
+++ generate/__init__.py
@@ -11,3 +11,9 @@ IMAGES = ['%s/resources/1.jpg' % script_path,
           '%s/resources/7.jpg' % script_path]
 
 VIDEOS = ['%s/resources/1.flv' % script_path,]
+
+PDFS = ['%s/resources/blank_pdf_document.pdf' % script_path,]
+
+CSVS = ['%s/resources/blank_csv.csv' % script_path,]
+
+TXTS = ['%s/resources/blank_text_document.txt' % script_path,]

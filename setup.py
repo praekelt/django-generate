9e252dc3adecab0789941c193a404a34d2e110c3
shaunsephton <shaun@praekeltconsulting.com>
2010-11-19 11:02:31 +0200

    versioned

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-generate',
-    version='dev',
+    version='mobiletv.0.0.1',
     description='Django slightly smarter than fixtures content generation app.',
     author='Praekelt Consulting',
     author_email='dev@praekelt.com',
     url='https://github.com/praekelt/django-generate',
     packages = find_packages(),
     include_package_data=True,
+    zip_safe=False,
 )

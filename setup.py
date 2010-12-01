e2583a187d9d4be804530bc82dee24e3f98e6cac
shaunsephton <shaun@praekeltconsulting.com>
2010-12-01 10:56:05 +0200

    corrected version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-generate',
-    version='mobiletv.0.0.1',
+    version='mtv.generic.0.0.1',
     description='Django slightly smarter than fixtures content generation app.',
     author='Praekelt Consulting',
     author_email='dev@praekelt.com',
